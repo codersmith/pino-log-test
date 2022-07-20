b9557c834df449b3f2b99ec71ea67fb7f215c863
Silas Smith <silas.smith@gmail.com>
2022-07-20 09:46:31 -0700

    Remove process.stdout

--- logger-pino.js
+++ logger-pino.js
@@ -2,8 +2,7 @@ const pino = require('pino')
 
 const _l = pino({
   timestamp: pino.stdTimeFunctions.isoTime,
-}, process.stdout)
-
+})
 const logger = _l.child({ logger: 'pino' })
 
 module.exports = {
