89bcb241a59ed6098f59bf1ff81de552e2ab9622
Silas Smith <silas.smith@gmail.com>
2022-07-24 14:19:00 -0700

    Intermediate checkin

--- logger-pino.js
+++ logger-pino.js
@@ -2,7 +2,7 @@ const pino = require('pino')
 
 const _l = pino({
   timestamp: pino.stdTimeFunctions.isoTime,
-})
+}, process.stdout)
 const logger = _l.child({ logger: 'pino' })
 
 module.exports = {
