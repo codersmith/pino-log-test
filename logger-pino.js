64e0529c8e106f30855552a81168544a1c33886c
Silas Smith <silas.smith@gmail.com>
2022-07-25 09:43:46 -0700

    Hide hostname

--- logger-pino.js
+++ logger-pino.js
@@ -2,6 +2,11 @@ const pino = require('pino')
 
 const _l = pino({
   timestamp: pino.stdTimeFunctions.isoTime,
+  formatters: {
+    bindings(bindings) {
+      return { pid: bindings.pid }
+    },
+  },
 }, process.stdout)
 
 const logger = _l.child({ logger: 'pino' })
